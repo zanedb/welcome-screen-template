ab19f660eecae5ade3e4c8c0402bacc8fe7652b4
Zane <webmaster@zaneweb.org>
2017-06-17 16:03:50 -0700

    Initial commit

---
+++ app/src/main/java/org/zaneweb/welcomescreentemplate/Application.java
@@ -0,0 +1,30 @@
+package org.zaneweb.welcomescreentemplate;
+
+import org.zaneweb.welcomescreentemplate.utils.Prefs;
+
+public class Application extends android.app.Application {
+
+    private Prefs prefs;
+    private static Application app;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        app = this;
+        prefs = new Prefs(this);
+    }
+
+    public static Application getApp() {
+        return app;
+    }
+
+    public Prefs getPrefs() {
+        return prefs;
+    }
+
+    public void setPrefs(Prefs prefs) {
+        this.prefs = prefs;
+    }
+
+}
